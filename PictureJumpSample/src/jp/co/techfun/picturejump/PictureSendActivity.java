ce1f821fef885491cc2b468ccbbf757033b43f62
kurata1016 <kurata1016@gmail.com>
2014-06-19 14:15:53 +0900

    second commit

--- PictureJumpSample/src/jp/co/techfun/picturejump/PictureSendActivity.java
+++ PictureJumpSample/src/jp/co/techfun/picturejump/PictureSendActivity.java
@@ -12,6 +12,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import jp.co.techfun.picturejump.bluetooth.PictureSendJob;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothDevice;
@@ -31,6 +32,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 // 画像送信画面Activity
+@SuppressLint("SimpleDateFormat")
 public class PictureSendActivity extends Activity {
 
 	// ギャラリー表示用リクエストコード
@@ -64,6 +66,13 @@ public class PictureSendActivity extends Activity {
 
 		// レイアウト設定ファイル指定
 		setContentView(R.layout.picture_send);
+
+		// 外部アプリケーションから画像を取得
+		if (Intent.ACTION_SEND.equals(getIntent().getAction())) {
+			Uri uri = getIntent().getParcelableExtra(Intent.EXTRA_STREAM);
+			picture = getImage(uri);
+			setPicture(picture);
+		}
 
 		// 「送信先選択」ボタンにリスナー設定
 		ImageButton ibtnSelectDevice = (ImageButton) findViewById(R.id.ibtn_select_device);
@@ -151,6 +160,17 @@ public class PictureSendActivity extends Activity {
 
 		// ボタンの使用可能状態を更新
 		updateButtonStatus();
+	}
+
+	// Uriから画像へ変換
+	private Bitmap getImage(Uri uri) {
+		try {
+			InputStream in = getContentResolver().openInputStream(uri);
+			return BitmapFactory.decodeStream(in);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	// onDestroyメソッド(画面破棄イベント)
@@ -245,7 +265,8 @@ public class PictureSendActivity extends Activity {
 		intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(imgPath)));
 		startActivity(intent);
 	}
-	
+
+	// 画像を保存してパス作成
 	private String savePictureInSDcard() {
 		// SDカードのルートディレクトリ取得
 		String dir = Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -255,21 +276,22 @@ public class PictureSendActivity extends Activity {
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss");
 		String imgPath = baseDir + File.separator + format.format(cal.getTime()) + ".png";
-		//画像をバイト型に変換
-		try{
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		picture.compress(CompressFormat.PNG, 100, os);
-		os.flush();
-		byte[] w = os.toByteArray();
-		os.close();
-		// 画像を一時的にSDカードに保存
-		FileOutputStream out = new FileOutputStream(imgPath);
-		out.write(w,0,w.length);
-		out.flush();
-		}catch(Exception e){
+		// 画像をバイト型に変換
+		try {
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			picture.compress(CompressFormat.PNG, 100, os);
+			os.flush();
+			byte[] w = os.toByteArray();
+			os.close();
+			// 画像を一時的にSDカードに保存
+			@SuppressWarnings("resource")
+			FileOutputStream out = new FileOutputStream(imgPath);
+			out.write(w, 0, w.length);
+			out.flush();
+		} catch (Exception e) {
 			Log.e("error", "image error");
 		}
-		
+
 		return imgPath;
 	}
 
